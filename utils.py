210b4757174a47bedeffe64e5c69a57b2820be1a
ded <stepanov.eltech@gmail.com>
2016-11-10 00:24:57 +0300

    add streaming

---
+++ utils.py
@@ -0,0 +1,20 @@
+def getch():
+	try:
+		# for Windows-based systems
+		import msvcrt # If successful, we are on Windows
+		return msvcrt.getch()
+
+	except ImportError:
+		# for POSIX-based systems (with termios & tty support)
+		import tty, sys, termios  # raises ImportError if unsupported
+
+		fd = sys.stdin.fileno()
+		oldSettings = termios.tcgetattr(fd)
+
+		try:
+			tty.setraw(fd)
+			answer = sys.stdin.read(1)
+		finally:
+			termios.tcsetattr(fd, termios.TCSADRAIN, oldSettings)
+
+		return answer
