d6d3fee99829d2f6fd243e34628759cbb77e471a
ded <stepanov.eltech@gmail.com>
2016-11-10 00:25:54 +0300

    add streaming

--- player.py
+++ player.py
@@ -1,6 +1,11 @@
+#! /opt/anaconda3/bin/python3
+
 import requests
-from subprocess import call
+import subprocess
 import config
+import sys
+import os
+from utils import getch
 
 query = 'https://api.vk.com/method/audio.get?owner_id={}&access_token={}'.format(config.owner_id, config.token)
 
@@ -8,18 +13,16 @@ r = requests.post(query)
 
 all_tracks = [[x['artist'], x['title'], x['url'].split('?')[0]] for x in r.json()['response'][1:]]
 
-for track in all_tracks[:3]:
-	with open('%s.mp3' % track[1], 'wb') as f:
-		response = requests.get(track[2], stream=True)
-
-		if not response.ok:
-			print('ERROR')
-
-		for block in response.iter_content(1024):
-			f.write(block)
-			print('.', end='')
-
-		print('OK')
-
-	#tmp = call(['ffplay', '-nodisp', '-autoexit', '%s.mp3' % track[1]])
-	tmp = call(['mpg321', '-o', 'alsa', '%s.mp3' % track[1]])
+for track in all_tracks[:]:
+	tmp = subprocess.Popen(['ffplay', '-nodisp', '-autoexit', track[2]], stderr=open(os.devnull, 'wb'))
+	print('{}\n{} - {}'.format('~'*20, track[0], track[1]))
+	print('next(q)/exit(x)')
+	while tmp.poll() == None:
+		x = getch()
+		if x == 'q':
+			tmp.kill()
+			break
+		elif x == 'x':
+			tmp.kill()
+			sys.exit()
+			break
