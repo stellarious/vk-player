9fe6d19f9c3ad289fbeff2b2fd8238eec1e2bd65
ded <stepanov.eltech@gmail.com>
2016-11-12 15:08:41 +0300

    add non-blocking input

--- player.py
+++ player.py
@@ -5,15 +5,17 @@ import subprocess
 import config
 import sys
 import os
-from utils import getch
+from utils import timeoutgetch
 import threading
 import time
+import psutil
 
 def wrapper(func, res):
-    res.append(func())
+	del res[:]
+	res.append(func())
 
 def get_tracks():
-	query = 'https://api.vk.com/method/audio.get?owner_id={}&access_token={}'.format(config.owner_id, config.token)
+	query = 'https://api.vk.com/method/audio.get?owner_id={}&access_token=41398e48456b22943fe354e99c221f8b0a3f9b63cf944ba72c9a9dbc997549816c520dc2bb54ac090d9ae'.format(config.owner_id)
 	r = requests.post(query)
 	return  [[x['artist'], x['title'], divmod(x['duration'], 60), x['url'].split('?')[0]] for x in r.json()['response'][1:]]
 
@@ -32,11 +34,12 @@ all_tracks = res[0]
 pointer = 0
 while True:
 	track = all_tracks[pointer]
-	tmp = subprocess.Popen(['ffplay', '-nodisp', '-autoexit', track[3]], stderr=open(os.devnull, 'wb'))
+	tmp = subprocess.Popen(['{}ffplay'.format(config.ffmpeg_path), '-nodisp', '-autoexit', track[3]], stderr=open(os.devnull, 'wb'))
+	ps_process = psutil.Process(pid=tmp.pid)
 	print('{}\n{} - {} [{}:{}]'.format('~'*20, track[0], track[1], track[2][0], track[2][1]))
 	print('prev(q)/next(w)/exit(x)')
 	while not tmp.poll():
-		x = getch() #blocking input
+		x = timeoutgetch()
 		if x == 'q':
 			tmp.kill()
 			if pointer < 1: pointer = 1
--- utils.py
+++ utils.py
@@ -1,20 +1,69 @@
-def getch():
-	try:
-		# for Windows-based systems
-		import msvcrt # If successful, we are on Windows
-		return msvcrt.getch()
+import os, time
 
-	except ImportError:
-		# for POSIX-based systems (with termios & tty support)
-		import tty, sys, termios  # raises ImportError if unsupported
+if os.name == 'nt':
+	import msvcrt
+else:
+	import sys
+	import termios
+	import atexit
+	from select import select
 
-		fd = sys.stdin.fileno()
-		oldSettings = termios.tcgetattr(fd)
 
-		try:
-			tty.setraw(fd)
-			answer = sys.stdin.read(1)
-		finally:
-			termios.tcsetattr(fd, termios.TCSADRAIN, oldSettings)
+class KBHit:	
+	def __init__(self):
+	# Creates a KBHit object that you can call to do various keyboard things.
+		if os.name == 'nt':
+			pass
+		
+		else:
+			# Save the terminal settings
+			self.fd = sys.stdin.fileno()
+			self.new_term = termios.tcgetattr(self.fd)
+			self.old_term = termios.tcgetattr(self.fd)
+		
+			# New terminal setting unbuffered
+			self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+			termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+		
+			# Support normal-terminal reset at exit
+			atexit.register(self.set_normal_term)
 
-		return answer
+	def set_normal_term(self):
+		if os.name == 'nt':
+			pass
+		else:
+			termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+
+	def getch(self):
+		''' Returns a keyboard character after kbhit() has been called.
+			Should not be called in the same program as getarrow().
+		'''
+
+		s = ''
+
+		if os.name == 'nt':
+			return msvcrt.getch().decode('utf-8')
+		
+		else:
+			return sys.stdin.read(1)
+		
+
+	def kbhit(self):
+		if os.name == 'nt':
+			return msvcrt.kbhit()
+		else:
+			dr,dw,de = select([sys.stdin], [], [], 0)
+			return dr != []
+
+def timeoutgetch (timeout = 0.01):
+	kb = KBHit()
+	char = None
+	startTime = time.time()
+
+	while time.time() - startTime < timeout:
+		if kb.kbhit():
+					char = kb.getch()
+
+	kb.set_normal_term()
+	return char
